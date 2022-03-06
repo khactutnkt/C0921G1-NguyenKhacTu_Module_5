690bf1898e285107db11a1e297ddf8e8fd7809e2
khactutnkt <khactut@gmail.com>
2022-03-06 11:51:43 +0700

    06/03/2021

--- ss3-angular_overview/bai_tap/angular-bai-tap-ss3/src/app/app.module.ts
+++ ss3-angular_overview/bai_tap/angular-bai-tap-ss3/src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 import {FormsModule} from '@angular/forms';
-import { PetComponent } from './pet/pet.component';
-import { ProductComponent } from './product/product.component';
+import { ColorComponent } from './color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FontSizeEditorComponent,
-    PetComponent,
-    ProductComponent
+    CalculatorComponent,
+    ColorComponent
   ],
     imports: [
         BrowserModule,
---
+++ ss3-angular_overview/bai_tap/angular-bai-tap-ss3/src/app/calculator/calculator.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  calculation: string;
+  num1 = 0;
+  num2 = 0;
+  result: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  calculator() {
+    switch (this.calculation) {
+      case '+':
+        this.result = this.num1 + this.num2;
+        break;
+      case '-':
+        this.result = this.num1 - this.num2;
+        break;
+      case '*':
+        this.result = this.num1 * this.num2;
+        break;
+      case '/':
+        this.result = this.num1 / this.num2;
+    }
+  }
+
+
+}
